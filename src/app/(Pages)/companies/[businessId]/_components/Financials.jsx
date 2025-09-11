266b2b4c747d4b1c3545354d293569acb895d789
Shahid Raza <engineerbro0098@gmail.com>
2025-09-11 12:50:29 +0530

    resolve issue in financial component

--- src/app/(Pages)/companies/[businessId]/_components/Financials.jsx
+++ src/app/(Pages)/companies/[businessId]/_components/Financials.jsx
@@ -6,7 +6,7 @@ export default function FinancialsSection({ financials }) {
       <section id="financials" >
         <h2 className="text-xl font-bold mb-4 text-center">Financial Performance</h2>
         <div className="grid  md:grid-cols-4 gap-4">
-          <div ><MetricsCard financials={financials[0].revenue} title={"Revenue"}/></div>
+          <div ><MetricsCard financials={financials[0]?.revenue} title={"Revenue"}/></div>
           <div ><MetricsCard financials={financials[0]?.netProfit} title={"Net Profit"}/></div>
           {/* <div className="bg-white p-4 shadow rounded-lg">Profit Margin: {financials[0]?.profitMargin}%</div>
           <div className="bg-white p-4 shadow rounded-lg">Operating Expenses: ₹{financials[0]?.operatingExpenses}M</div> */}
